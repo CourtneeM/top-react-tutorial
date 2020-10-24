eb05da8168b08385e5096135acd2cffc28bac4c5
Courtnee McCollum <courtneemeaghan@gmail.com>
2020-10-24 16:21:07 -0500

    Add component files to project

--- cv-project/src/App.js
+++ cv-project/src/App.js
@@ -1,4 +1,7 @@
 import './App.css';
+import Personal from './components/Personal.js';
+import Education from './components/Education.js';
+import Experience from './components/Experience.js';
 
 function App() {
   return (
