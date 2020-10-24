b5e77a178817157ddbac119fd879e81f97e7b9c9
Courtnee McCollum <courtneemeaghan@gmail.com>
2020-10-23 19:35:43 -0500

    Set up handleClick to concat input to task array

--- handle-inputs-and-render-lists/src/App.js
+++ handle-inputs-and-render-lists/src/App.js
@@ -7,18 +7,47 @@ class App extends Component {
     super(props);
 
     this.state = {
-
+      input: "",
+      tasks: ['clean'],
     }
   }
 
+  handleInput = (e) => {
+    const tasks = this.state.tasks;
+
+    this.setState({
+        input: e.target.value,
+    });
+  }
+
+  handleClick = () => {
+    this.setState((currentState) => { 
+      return {
+        tasks: currentState.tasks.concat('yolooo')
+      }
+    });
+    
+    console.log(this.state);
+    this.setState({
+      input: "",
+    });
+
+
+  }
 
 
   render () {
     return (
       <div className="App">
-        <h3>Add a Task:</h3>
-        <input type="text" />
-        <button>Add</button>
+        <div className="input-task">
+          <h3>Add a Task:</h3>
+          <input type="text" onChange={this.handleInput} value={this.state.input}/>
+          <button onClick={this.handleClick}>Add</button>
+        </div>
+        <div className="display-tasks">
+          <h3>Tasks:</h3>
+          <Overview />
+        </div>
       </div>
     );
   }
