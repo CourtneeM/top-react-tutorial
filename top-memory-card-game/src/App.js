ff8943e403d10f56e5667747d65fa72937b2db32
Courtnee McCollum <courtneemeaghan@gmail.com>
2020-11-02 13:48:40 -0600

    Set up Score component and style

--- top-memory-card-game/src/App.js
+++ top-memory-card-game/src/App.js
@@ -1,10 +1,15 @@
-import './App.css';
+import React, {useState} from 'react';
 import Header from './Components/Header.js';
+import Score from './Components/Score.js';
+import './App.css';
 
 function App() {
+  const [scores, getScores] = useState([{currentScore: 0}, {maxScore: 0}]);
+
   return (
     <div className="App">
       <Header />
+      <Score scores={scores}  />
     </div>
   );
 }
--- top-memory-card-game/src/Components/Score.js
+++ top-memory-card-game/src/Components/Score.js
@@ -1,12 +1,20 @@
 import React, {useState} from 'react';
+import '../Styles/Score.css'
 
-function Score() {
-
+function Score({scores}) {
+  const [currentScore, maxScore] = scores;
 
 
   return (
     <div className="score-container">
-
+      <div className="current-score-container">
+        <p>Current Score</p>
+        {<p>{scores[0].currentScore}</p>}
+      </div>
+      <div className="max-score-container">
+        <p>Max Score</p>
+        <p>{scores[1].maxScore}</p>
+      </div>
     </div>
   )
 }
